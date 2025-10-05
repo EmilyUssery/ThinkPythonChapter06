7e118f0be7d9f3800b93082da5875933299b12cf
eussery122 <eussery122@mucr.redwoods.edu>
2025-10-04 21:37:40 -0700

    i hope i did it

---
+++ excise6.py
@@ -0,0 +1,39 @@
+import math
+
+def absolute_value(x):
+    if x < 0:
+        return -x
+    else:
+        return x
+
+
+def absolute_value2(x):
+    return -x if x < 0 else x
+
+
+def absolute_value_clean(x):
+    if x < 0:
+        return -x
+    else:
+        return x
+
+
+def absolute_value_clean(x):
+    if x < 0:
+        return -x
+    else:
+        return x
+
+def is_divisible(x, y):
+    if x % y == 0 or x % y == 0:
+        return True
+    else:
+        return False
+
+
+
+
+
+def distance(x1, y1, x2, y2):
+    return math.hypot(x2 - x1, y2 - y1)
+
